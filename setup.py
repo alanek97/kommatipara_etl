b6b4c905395ba817180c2a743bf84e5d496db45e
Alan <alan.sk97@outlook.com>
2023-09-26 20:36:00 +0200

    modified:   .gitignore

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+from setuptools import find_packages, setup
 
-from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='etl_kommatipara',
     packages=find_packages(where='src',include=['utils', 'etl_source_code']),
-    package_dir={"": "src"}
+    package_dir={"": "src"},
+    install_requires=requirements
 )
